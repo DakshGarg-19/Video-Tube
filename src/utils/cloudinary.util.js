38c9f047d6185df2bf6e9b7f3052adaeaf903d77
Daksh Garg <dakshgarg64@gmail.com>
2025-12-02 16:04:30 +0530

    Creating multer middleware and cloudinary utility module

---
+++ src/utils/cloudinary.util.js
@@ -0,0 +1,29 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+// Configuration
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+// Upload an image on to cloudinary
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
+
+    // upload file on cloudinary
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+    // successful upload of file on cloudinary
+    console.log("File has been uploaded to Cloudinary", response.url);
+    return response;
+  } catch (error) {
+    fs.unlinkSync(localFilePath); // removes the locally saved temporary file as the upload operation failed
+    return null;
+  }
+};
+
+export { uploadOnCloudinary };
